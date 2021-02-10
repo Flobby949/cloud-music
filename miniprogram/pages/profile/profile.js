308bc80fa48d19ee9b6d4752661b9e50980728fc
Flobby <2541226493@qq.com>
2021-02-10 19:21:37 +0800

    个人页面数据栏

--- miniprogram/pages/profile/profile.js
+++ miniprogram/pages/profile/profile.js
@@ -1,5 +1,7 @@
 // pages/profile/profile.js
-
+const app = getApp()
+let songNum = 0
+let blogNum = 0
 Page({
   data: {
     songNum: 0,
@@ -7,10 +9,32 @@ Page({
   },
 
   onLoad: function () {
+    this._getUserData()
+  },
+
+  onShow: function () {
+    this._getUserData()
+  },
+
+  _getUserData() {
+    const playHistory = wx.getStorageSync(app.globalData.openid)
+    songNum = playHistory.length
+    wx.cloud.database().collection('blog')
+      .where({
+        _openid: app.globalData.openid
+      })
+      .get()
+      .then((res) => {
+        blogNum = res.data.length
+        this.setData({
+          songNum,
+          blogNum,
+        })
+      })
 
   },
 
-  onTapQrCode(){
+  onTapQrCode() {
     wx.showLoading({
       title: '生成中',
     })
